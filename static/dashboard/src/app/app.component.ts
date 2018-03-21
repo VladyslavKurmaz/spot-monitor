a7e1f512922df387a31f9798b1e895eeca3148df
vladyslav.kurmaz <vladyslav.kurmaz@globallogic.com>
2018-03-21 14:37:28 +0000

    /cameras get request

--- static/dashboard/src/app/app.component.ts
+++ static/dashboard/src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 
 import { Camera } from './models/camera'
 
@@ -8,7 +9,37 @@ import { Camera } from './models/camera'
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
-  cameras: Camera[] = [{ip:'127.0.0.1:9900',user:'admin',password:'admin',endpoint:'9990'}];
+  errorMessage = '';
+  cameras: Camera[] = [];//[{ip:'127.0.0.1:9900',user:'admin',password:'admin',endpoint:'9990'}];
   addNew: Boolean = false;
+
+  constructor(private http: HttpClient){
+  }
+
+  ngOnInit() {
+    this.http.get('http://46.101.7.84:8000/cameras')
+      .subscribe(
+        data => this.cameras,
+        error => this.handleError(error)
+      );
+  }
+
+  private handleError(error: HttpErrorResponse) {
+    console.log('Error');
+    console.log(error);
+    if (error.error instanceof ErrorEvent) {
+      // A client-side or network error occurred. Handle it accordingly.
+      console.error('An error occurred:', error.error.message);
+      this.errorMessage = error.error.message;
+    } else if (error.error instanceof ProgressEvent) {
+    } else {
+      // The backend returned an unsuccessful response code.
+      // The response body may contain clues as to what went wrong,
+      let msg = `Backend returned code ${error.status}, ` +
+                `body was: ${error.error}`;
+      console.error(msg);
+      this.errorMessage = msg;
+    }
+  };
+
 }
